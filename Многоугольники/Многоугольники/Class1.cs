45a00779204d20344bd41c0c42003b7265c13fb1
KR4FTWERKK <43827125+KR4FTWERKK@users.noreply.github.com>
2018-12-05 19:59:53 +0300

    Add files via upload

---
+++ Многоугольники/Многоугольники/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Многоугольники
+{
+    public abstract class Shape
+    {
+        protected static int R; //радиус
+        public abstract void Draw(); 
+        protected int x;
+        protected int y;
+    }
+    public class Circle: Shape
+    {
+        public override void Draw()
+        {
+          
+        }
+
+    } 
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+        
+        }
+    }
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            
+        }
+    }
+}
---
+++ Многоугольники/Многоугольники/Shape.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Многоугольники
+{
+    public abstract class Shape
+    {
+        protected bool Flag = false;
+        protected bool Deletement_Flag = false;
+        protected SolidBrush brush;
+        protected static Color Outline;
+        protected static Color Filler;
+        protected static int r; //радиус
+        public abstract void Draw(Graphics x);
+        public abstract void FlagUpdater(int x1, int x2);
+        protected int x;
+        protected int y;
+        public bool FLAG
+        {
+            get
+            {
+                return Flag;
+            }
+            set
+            {
+                Flag = value;
+            }
+        }
+        public bool DO_NOT_DELETE_FLAG
+        {
+            get
+            {
+                return Deletement_Flag;
+            }
+            set
+            {
+                Deletement_Flag = value;
+            }
+        }
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+        public Color FillColor
+        {
+            get
+            {
+                return Filler;
+            }
+            set
+            {
+                Filler = value;
+            }
+        }
+        public Color OutlineColor
+        {
+            get
+            {
+                return Outline;
+            }
+            set
+            {
+                Outline = value;
+            }
+        }
+        public int R
+        {
+            get
+            {
+                return r;
+            }
+            set
+            {
+                r = value;
+            }
+        }
+        static Shape()//статический конструктор
+        {
+            Outline = Color.Blue;
+            r = 20;
+            Filler = Color.Red;
+        }
+        public Shape(int x, int y)//динамический конструктор
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
